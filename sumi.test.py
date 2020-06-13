ff86486cbb4d6975d058b1a20dc3c38cc3799e36
Ayokunle Odutayo <odunet2000@yahoo.com>
2020-06-13 22:04:45 +0100

    Updated the test file, removing need to load arguments_4

--- sumi.test.py
+++ sumi.test.py
@@ -5,9 +5,9 @@ class SimpleTest(unittest.TestCase):
 
     # Returns True or False.
     def test(self):
-        z,y = sumi.ave([4,5,6,7,8])
+        z,y = sumi.ave([10,11,12])
         print(z,y)
-        self.assertTrue(z,7)
+        self.assertEqual(z,11)
 
 if __name__ == '__main__':
     unittest.main()
